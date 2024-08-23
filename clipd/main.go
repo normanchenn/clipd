dc281824fc13615915b1cd3c198e941dfc7dca83
Norman Chen <njchen@uwaterloo.ca>
2024-08-22 23:10:11 -0230

    remove everything

--- clipd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/normanchenn/clipd/clipd/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- restart.sh
+++
@@ -1,5 +0,0 @@
-\launchctl unload ~/Library/LaunchAgents/clipd.plist
-if [ -f /tmp/clipd.sock ]; then
-	rm /tmp/clipd.sock
-fi
-\launchctl load -w ~/Library/LaunchAgents/clipd.plist
